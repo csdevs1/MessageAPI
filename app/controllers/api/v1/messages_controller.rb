c88ef7368d5b462dfc5c0a446c6d1ea87b89d9a1
csdevs1 <csdevs1@gmail.com>
2016-03-12 13:51:56 -0430

    Improved API

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,9 +3,24 @@ module Api
         class MessagesController < ApplicationController
             respond_to :json
             
+            # Full list of status codes can be found at https://en.wikipedia.org/wiki/List_of_HTTP_status_codes, but you’re most likely to use only limited subset of them:
+
+            # 200 - :ok
+            # 204 - :no_content
+            # 400 - :bad_request
+            # 403 - :forbidden
+            # 401 - :unauthorized
+            # 404 - :not_found
+            # 410 - :gone
+            # 422 - :unprocessable_entity
+            # 500 - :internal_server_error
+            
             def index
                 @messages = Message.all
                 respond_with @messages
+                # @hola = {hola: 'hola', message: @messages}
+                # respond_with @hola
+                # Use this ^ code without the views/api layout
             end
             
             def show
@@ -14,7 +29,10 @@ module Api
             end
             
             def create 
-                respond_with Message.create(message_params)
+                Message.create(message_params)
+              #  @hola = {saved: 'hola'}
+               # respond_with @hola
+                render json: {message: 'Great'}, status: 200
             end
             
             def update
